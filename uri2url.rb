90f6e8910201f9b50a300767322d2bb4b5644f92
Việt Anh Trần <henrytran@192.168.1.179.non-exists.ptr.local>
2025-06-05 13:00:30 +0700

    Update SHA256 for v1.0.0

--- uri2url.rb
+++ uri2url.rb
@@ -2,7 +2,7 @@ class Uri2url < Formula
   desc "Convert custom URI schemes into shareable URLs"
   homepage "https://github.com/henrytran1803/tools-uri2url"
   url "https://github.com/henrytran1803/tools-uri2url/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "bdc224528c2beb60f553fce1e325e8aa780916e15e94e934f04a862b4f6610c8"
   license "MIT"
 
   def install
